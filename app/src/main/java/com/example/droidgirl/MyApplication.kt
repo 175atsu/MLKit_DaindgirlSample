d6aed38b6cc202a361fa4b96b5e23a1ce9e91616
Atsu <astu.i.0601@gmail.com>
2019-12-14 23:18:43 +0900

    最初のコミット

---
+++ app/src/main/java/com/example/droidgirl/MyApplication.kt
@@ -0,0 +1,13 @@
+package com.example.droidgirl
+
+import android.app.Application
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraXConfig
+
+class MyApplication : Application(), CameraXConfig.Provider {
+
+    override fun getCameraXConfig(): CameraXConfig {
+        return Camera2Config.defaultConfig(this)
+    }
+}
+
